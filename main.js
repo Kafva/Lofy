f18d62d557ed9ba9c012440297b0ca26dd3818e1
Kafva <Martensson.Jonas98@gmail.com>
2020-08-25 13:59:03 +0200

    -

---
+++ main.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+//********* DEBUGGING **********/
+//		tshark -i en0 -Y "http.request || http.response" tcp port 443 or 80
+//		curl http://localhost:3443/callback?state=333&code=deadbeef
+//		Object.keys() ~ dir()
+//******************************/
+
+//******* CONSTANTS ************/
+const CONSTS = 
+{
+	WEB_SERVICE_PORT: 3443,
+	STATE_STR_LENGTH: 16,
+	DIR_ROOT: 'public',
+
+	client_id: '08c759bd98d149f58ce04c9caabb0f97', 
+	auth_endpoint: 'https://accounts.spotify.com/authorize?',
+	base_uri: 'http://localhost:3443',
+	redirect_uri: `http://localhost:3443/callback`,
+
+	// Authorizations required by the app
+	// https://developer.spotify.com/documentation/general/guides/scopes/
+	scope: 'playlist-read-private streaming',
+	state_cookie_key: 'spotify_auth_state'
+}
+
+//*************************************/
+
+// Import the functions neccessary into the functions object (passing along the constants)
+// Some external modules like queryString are also imported from here
+const functions = require('./serverFunctions')(CONSTS);
+const path = require('path');
+
+//********* FASTIFY SETUP ************/
+
+// Fastify is an alternative to express, with fastify-static being
+// requiried to serve static files, i.e. access to reply.sendFile()
+const fastify = require('fastify')({ logger: true });
+const fastify_static = require('fastify-static');
+
+// Set the static content directory root
+// The register method allows the user to extend functionality with plugins. 
+// A plugin can be a set of routes, a server decorator etc. in this case 
+// it provides static file serving
+fastify.register( fastify_static,
+{
+	root: path.join(__dirname, CONSTS.DIR_ROOT),
+	prefix: '/',
+})
+
+// Register the CORS (Cross-origin resource sharing) plugin
+// When issuing a request in JS from a page the HTTP headers 'Origin:'
+// is accepted by default (Same origin requests)
+
+// To send requests to other sites we need to explicitly enable these with the
+// 'Access-Control-Allow-Origin:' field(s) in our HTTP messages  (can be set to wildcard).
+fastify.register(require('fastify-cors'), { origin: "*" })
+
+// TEMPLATE ENGINE
+// Plugin for template engine (i.e. dynamic HTML) compatiblity with fastify
+fastify.register(require('point-of-view'), 
+{
+	engine: 
+	{
+		// The template engine used (EJS = Embedded JavaScript)
+		ejs: require('ejs')
+	}
+})
+
+// COOKIE support
+// This plugin gives us access to reply.cookies and request.cookies
+fastify.register(require('fastify-cookie'), {
+	secret: functions.stateString(CONSTS.STATE_STR_LENGTH), 
+	parseOptions: {} 
+})
+
+//************************************/
+
+// Setup the routes
+require("./routes")(fastify,functions,CONSTS);
+
+// Start the server
+require("./server")(fastify,CONSTS);
+
--- serverFunctions.js
+++ serverFunctions.js
@@ -1,31 +1,38 @@
 // Useful when creating REST API querys
 const queryString = require('querystring');
 
-module.exports = 
-// Functions in `module.exports` will be available inside the required() object that is imported in server.js 
+module.exports = (CONSTS) => 
+// Functions in `module.exports` will be available inside the required() object that is imported in server.js
+// Since we assign a function to module.exports in this case we need to RETURN a dictionary
+// of the functions we define 
 {
-    
-    stateString: (length) =>
-    // Produce an alphanumeric random string
+    var functions = 
     {
-        let bases = [ 48, 65, 97 ];
-        let cnts  = [ 9, 25, 25  ];
-        let index = -1; let str = "";
-
-        for (let i=0; i<length; i++)
+        stateString: (length) =>
+        // Produce an alphanumeric random string
         {
-            // If we multiply by 3 there is a minimal chance that we get a index out of bounds
-            index = Math.floor(Math.random() * 2.9999999);
-            str += String.fromCharCode( Math.floor(Math.random()*cnts[index] + bases[index]) );
-        }
-        
-        return str;
-    },
+            let bases = [ 48, 65, 97 ];
+            let cnts  = [ 9, 25, 25  ];
+            let index = -1; let str = "";
 
-    errorRedirect: (reply,base_uri,msg) =>
-    {
-        reply.redirect( `${base_uri}/error?` + 
-            queryString.stringify( {error: msg })
-        ); 
-    }
+            for (let i=0; i<length; i++)
+            {
+                // If we multiply by 3 there is a minimal chance that we get a index out of bounds
+                index = Math.floor(Math.random() * 2.9999999);
+                str += String.fromCharCode( Math.floor(Math.random()*cnts[index] + bases[index]) );
+            }
+            
+            return str;
+        },
+
+        errorRedirect: (reply,msg) =>
+        {
+            reply.redirect( `${CONSTS.base_uri}/error?` + 
+                queryString.stringify( {error: msg })
+            ); 
+        }
+
+    };
+    
+    return functions;
 };
